b12f9db5a651e9c94d857d6c84ed39beade714f2
kietzxc1 <21130413@st.hcmuaf.edu.vn>
2026-05-28 17:58:34 +0700

    feat(backend): bootstrap spring boot and base config

---
+++ back-end/src/main/java/nlu/com/app/BackEndBookStoreApplication.java
@@ -0,0 +1,17 @@
+package nlu.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@EnableScheduling
+public class BackEndBookStoreApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackEndBookStoreApplication.class, args);
+  }
+
+}
